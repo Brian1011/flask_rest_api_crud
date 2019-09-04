1bb420491527c6ce24219541b8335d2c02c05656
Brian1011 <mutindabrian26@gmail.com>
2019-09-04 18:28:32 +0300

    add extras

--- app.py
+++ app.py
@@ -59,7 +59,6 @@ def add_product():
     new_product = Product(name, description, price, qty)
     db.session.add(new_product)
     db.session.commit()
-
     return product_schema.jsonify(new_product)
 
 
